a9e5961fb48663b29b8ff6d6ac0321dbb067ed38
ilinajovanoskaa <jovanoskaa074@gmail.com>
2025-01-13 12:54:50 +0100

    Hangman

---
+++ winter homework/Hangman/scripts/word-list.js
@@ -0,0 +1,8 @@
+const wordList = [
+  { word: "Titanic", hint: "A romantic tragedy set on a famous ship" },
+  { word: "Avatar", hint: "A visually stunning alien world adventure" },
+  { word: "Inception", hint: "A mind-bending heist within dreams" },
+  { word: "Jaws", hint: "A thriller about a killer shark" },
+  { word: "Gladiator", hint: "A tale of revenge in ancient Rome" },
+  { word: "Frozen", hint: "An icy kingdom and a magical queen" },
+];
